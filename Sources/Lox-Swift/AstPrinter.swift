d675e491962e369907531cbcb73a93c397193c36
ttdo19 <thuytrang19072001@gmail.com>
2024-09-09 19:43:49 -0500

    Update GenerateAst to generate Swift file

---
+++ Sources/Lox-Swift/AstPrinter.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Trang Do on 9/8/24.
+//
+
+import Foundation
---
+++ Sources/Lox-Swift/Parser.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Trang Do on 9/9/24.
+//
+
+import Foundation
